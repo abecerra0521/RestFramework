0e935672dd9f53ffde2b5cf0bf7b5769d61de02c
Andres Becerra <abecerra@tracsa.com.mx>
2019-06-25 17:28:35 -0500

    endpoint for reservations for user

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from users.serializers import UserSerializer
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+from reservations.models import Reservation
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
@@ -16,3 +17,9 @@ class UserViewSet(viewsets.ModelViewSet):
     def get_user_token(self, request, pk=None):
         user = Token.objects.filter(key=pk).values().first()
         return Response(user)
+
+    
+    @action(methods=['get'], detail=True)
+    def reservations(self, request, pk=None):
+        reservations = Reservation.objects.filter(user_id=pk).values()
+        return Response(reservations)
